af89b4ec413ea9cb92a85e8bf9899279523d04b7
Billy Porter <wporter2@nd.edu>
2022-03-21 12:15:03 -0400

    Restructure 2

---
+++ layers.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+
+
+# Attention layer for words and sentences
+class AttentionWithContext(nn.Module):
+    def __init__(self):
+        super(AttentionWithContext, self).__init__()
+
+        self.W = torch.nn.Parameter(torch.empty(100, 100))
+        self.u = torch.nn.Parameter(torch.empty(100, ))
+        torch.nn.init.normal_(self.W, std=0.01)
+        torch.nn.init.normal_(self.u, std=0.01)
+
+    def forward(self, x, mask=None):
+        uit = torch.matmul(x, self.W)
+
+        uit = torch.tanh(uit)
+        ait = torch.matmul(uit, self.u)
+
+        a = torch.exp(ait)
+
+        a = a / torch.sum(a, axis=1, keepdim=True) + 1e-7
+        a = a.unsqueeze(-1)
+        weighted_input = x * a
+
+        return torch.sum(weighted_input, axis=1)
+
+
+# Hierarchical Attention Encoder
+class HierAttLayerEncoder(nn.Module):
+    def __init__(self, vocab_sz, embedding_dim, embedding_mat):
+        super(HierAttLayerEncoder, self).__init__()
+
+        self.emb_layer = nn.Embedding(vocab_sz, embedding_dim)
+        self.emb_layer.weights = torch.nn.Parameter(torch.from_numpy(embedding_mat))
+        self.l_lstm = nn.GRU(input_size=100, hidden_size=100, num_layers=2, bidirectional=True, batch_first=True)
+        self.l_dense = TimeDistributed(nn.Linear(in_features=200, out_features=100))
+        self.l_att = AttentionWithContext()
+
+    def forward(self, x):
+        embedded_sequences = self.emb_layer(x)
+        out, h = self.l_lstm(embedded_sequences)
+        out = self.l_dense(out)
+        out = self.l_att(out)
+        return out
+
+# Allows to apply a layer to every temporal slice of an input
+# Equivalent of Keras https://keras.io/api/layers/recurrent_layers/time_distributed/
+# Source: https://discuss.pytorch.org/t/any-pytorch-function-can-work-as-keras-timedistributed/1346/4
+class TimeDistributed(nn.Module):
+    def __init__(self, module, batch_first=True):
+        super(TimeDistributed, self).__init__()
+        self.module = module
+        self.batch_first = batch_first
+
+    def forward(self, x):
+        if len(x.size()) <= 2:
+            return self.module(x)
+        # Squash samples and timesteps into a single axis
+        x_reshape = x.contiguous().view(-1, x.size(-1))  # (samples * timesteps, input_size)
+
+        y = self.module(x_reshape)
+
+        # We have to reshape Y
+        if self.batch_first:
+            y = y.contiguous().view(x.size(0), -1, y.size(-1))  # (samples, timesteps, output_size)
+        else:
+            y = y.view(-1, x.size(1), y.size(-1))  # (timesteps, samples, output_size)
+
+        return y
+
+
+class DataWrapper():
+    def __init__(self, x_data, y_data):
+        self.x = x_data
+        self.y = y_data
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from model import WSTC
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
+from layers import DataWrapper
+import argparse
+from argparse import RawTextHelpFormatter
+
 
 class DataWrapper():
     def __init__(self, x_data, y_data):
@@ -14,53 +18,122 @@ class DataWrapper():
     def __getitem__(self, index):
         return self.x[index], self.y[index]
 
-def main():
-    print(torch. __version__)
 
-    # Constants
+def main():
+
+    # Add parser arguments
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+
+    ### Basic Settings ###
+    parser.add_argument("-m",
+                        "--method",
+                        choices=["p", "s", "e"],
+                        help="p: pretrain\ns: self-train\ne: eval",
+                        default="p")
+
+    ### Training Settings ###
+    # mini-batch size for both pre-training and self-training: 256 (default)
+    parser.add_argument('--batch_size', default=256, type=int)
+    # maximum self-training iterations: 5000 (default)
+    parser.add_argument('--maxiter', default=5e3, type=int)
+    # pre-training epochs: None (default)
+    parser.add_argument('--pretrain_epochs', default=200, type=int)
+    # self-training update interval: None (default)
+    parser.add_argument('--update_interval', default=100, type=int)
+    args = parser.parse_args()
+
+    ### Constants ###
     xshape = (12000, 10, 45)
     n_classes = 4
     argsmodel = 'rnn'
     vocab_sz = 67765
     word_embedding_dim = 100
-
-    # Load data
-    seed_docs_numpy = np.load('seed_docs.npy')
-    seed_label = np.load('seed_label.npy')
     embedding_mat = np.load('embedding_mat.npy')
 
-    # Process
-    seed_docs = torch.from_numpy(seed_docs_numpy)
-    seed_label = torch.from_numpy(seed_label)
-    # seed_docs = seed_docs.long()
-    seed_label = seed_label.type(torch.FloatTensor)
-    print('here')
+    ### Pretrain ###
+    if args.method == 'p':
+        # Load data
+        seed_docs_numpy = np.load('seed_docs.npy')
+        seed_label = np.load('seed_label.npy')
 
+        # Process
+        seed_docs = torch.from_numpy(seed_docs_numpy)
+        seed_label = torch.from_numpy(seed_label)
+        seed_label = seed_label.type(torch.FloatTensor)
 
-    # Process data
-    # train_data = DataWrapper(seed_docs, seed_label)
-    # train_loader = DataLoader(dataset=train_data, batch_size=1, shuffle=True)   
+        # Process data
+        train_data = DataWrapper(seed_docs, seed_label)
+        train_loader = DataLoader(dataset=train_data,
+                                  batch_size=1,
+                                  shuffle=True)
 
-    # Call model
-    # wstc = WSTC(input_shape=xshape, n_classes=n_classes, model=argsmodel,
-    #             vocab_sz=vocab_sz, embedding_matrix=embedding_mat, word_embedding_dim=word_embedding_dim)
+        # Call model
+        wstc = WSTC(input_shape=xshape,
+                    n_classes=n_classes,
+                    model=argsmodel,
+                    batch_size=args.batch_size,
+                    vocab_sz=vocab_sz,
+                    embedding_matrix=embedding_mat,
+                    word_embedding_dim=word_embedding_dim)
 
-    # wstc.pretrain(train_loader)
+        wstc.pretrain(train_loader, args.pretrain_epochs)
 
-    model = torch.load('model')
+    ### Self train ###
+    if args.method == 's':
 
-    y = np.load('new_y.npy')
-    x = np.load('new_x.npy')
-    train_data = DataWrapper(x, y)
-    self_train_loader = DataLoader(dataset=train_data, batch_size=1, shuffle=False)
+        # Load saved model
+        model = torch.load('model.pt')
 
-    wstc = WSTC(input_shape=xshape, classifier=model, n_classes=n_classes, model=argsmodel,
-                vocab_sz=vocab_sz, embedding_matrix=embedding_mat, word_embedding_dim=word_embedding_dim)
+        # # Load data
+        y = np.load('new_new_y.npy')
+        x = np.load('new_new_x.npy')
+        # print(x.shape)
+        train_data = DataWrapper(x, y)
+        self_train_loader = DataLoader(dataset=train_data,
+                                       batch_size=1,
+                                       shuffle=False)
 
-    wstc.self_train(self_train_loader, x, y)
+        # Call self training
+        wstc = WSTC(input_shape=xshape,
+                    classifier=model,
+                    n_classes=n_classes,
+                    model=argsmodel,
+                    batch_size=args.batch_size,
+                    vocab_sz=vocab_sz,
+                    embedding_matrix=embedding_mat,
+                    word_embedding_dim=word_embedding_dim)
 
+        wstc.self_train(self_train_loader,
+                        x,
+                        y,
+                        maxiter=args.maxiter,
+                        update_interval=args.update_interval)
 
+    if args.method == 'e':
 
+        # Load saved model
+        model = torch.load('model.pt')
+
+        # Load data
+        # model = None
+        y = np.load('new_new_y.npy')
+        x = np.load('new_new_x.npy')
+
+        test_data = DataWrapper(x, y)
+        test_loader = DataLoader(dataset=test_data,
+                                 batch_size=1,
+                                 shuffle=False)
+
+        wstc = WSTC(input_shape=xshape,
+                    classifier=model,
+                    n_classes=n_classes,
+                    model=argsmodel,
+                    batch_size=args.batch_size,
+                    vocab_sz=vocab_sz,
+                    embedding_matrix=embedding_mat,
+                    word_embedding_dim=word_embedding_dim)
+
+        wstc.test_load_deprecated(test_loader)
 
 
 if __name__ == "__main__":
